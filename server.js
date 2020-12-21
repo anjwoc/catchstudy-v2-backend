707f450e6c60c9f2ff75434f0218725a0327b934
anjwoc <anjwoc@naver.com>
2020-12-22 01:23:14 +0900

    change ports

--- server.js
+++ server.js
@@ -18,3 +18,27 @@ require("greenlock-express")
   // Serves on 80 and 443
   // Get's SSL certificates magically!
   .serve(app);
+
+function httpsWorker(glx) {
+  //
+  // HTTPS 1.1 is the default
+  // (HTTP2 would be the default but... https://github.com/expressjs/express/issues/3388)
+  //
+  // Get the raw https server:
+  var httpsServer = glx.httpsServer(null, function (req, res) {
+    res.end("Hello, Encrypted World!");
+  });
+
+  httpsServer.listen(4443, "0.0.0.0", function () {
+    console.info("Listening on ", httpsServer.address());
+  });
+
+  // Note:
+  // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
+  // (the ACME and http->https middleware are loaded by glx.httpServer)
+  var httpServer = glx.httpServer();
+
+  httpServer.listen(4480, "0.0.0.0", function () {
+    console.info("Listening on ", httpServer.address());
+  });
+}
